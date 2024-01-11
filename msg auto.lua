be9e22e2436395bad99e8ab05e7d7dc68d4a33e2
Pixe <52007751+pixe71@users.noreply.github.com>
2024-01-11 17:55:05 +0100

    Add files via upload
    English version

--- msg auto.lua
+++ msg auto.lua
@@ -1,12 +1,12 @@
 --Addon développer par Pixe
 
---Mise à jours à venir 
+--Upcoming updates
 
---Sujet de la mise à jours : Interface Graphique (PopUp En haut à droite esthéthique  :)
+--Subject of the update: Graphical Interface (PopUp Top right aesthetic) :)
 
 
 
-local oocMessage1 = "Afin d'être en contact régulier avec notre communauté, rejoignez notre discord : https://discord.gg/Y3H2sEEXq4"
+local oocMessage1 = "Text"
 
 local function sendOOCMessage1()
     RunConsoleCommand("say",.. oocMessage1)
@@ -14,22 +14,22 @@ end
 
 timer.Create("OOCMessageTimer1", 600, 0, sendOOCMessage1)
 
-local oocMessage2 = "Vous aimez le projet et voulez le soutenir ? Rendez-vous sur notre boutique : https://ash-purge.tebex.io/. L'équipe d'Ash-Purge vous remercie pour vos achats !"
+local oocMessage2 = "Text 2"
 
 local function sendOOCMessage2()
     RunConsoleCommand("say",  .. oocMessage2) 
 
 timer.Create("OOCMessageTimer2", 900, 0, sendOOCMessage2)
 
-print("Le script contenant les messages automatiques a été chargé correctement !")
+print("The script containing the automatic messages has been loaded correctly!")
 
---local oocMessage3 = "Ceci est un test" (Votre message)
+--local oocMessage3 = “This is a test” (Your message)
 
 --local function sendOOCMessage3()
  --   RunConsoleCommand("say",  .. oocMessage3)  
 --end
 
---timer.Create("OOCMessageTimer3", 1200, 0, sendOOCMessage3) (Délais avant chaque réaparitions dans le chat des différents message)
+--timer.Create("OOCMessageTimer3", 1200, 0, sendOOCMessage3) (Delays before each reappearance in the chat of different messages)
 
-print("Le script contenant les messages automatiques a été chargé correctement !")
+print("The script containing the automatic messages has been loaded correctly")
 
